020976664d261b0ef797ee14262c004adb707878
Glitch <glitchthefurry@gmail.com>
2023-03-06 22:50:24 -0600

    Fix URL encoding in logout redirects

--- includes/header.php
+++ includes/header.php
@@ -34,7 +34,8 @@ if (isset($_GET['logout'])) {
     //header('Location: /');
 	
 	if (isset($_SESSION['accessToken'])){
-		header("Refresh:0; url={$OAUTH_CONCAT_BASE_URL}/oauth/logout?next={urlencode($CANONICAL_URL)}&client_id={$OAUTH_CLIENT_ID}&access_token=" . $_SESSION['accessToken'], true, 303);
+        $encoded_url = urlencode($CANONICAL_URL);
+		header("Refresh:0; url={$OAUTH_CONCAT_BASE_URL}/oauth/logout?next=$encoded_url&client_id={$OAUTH_CLIENT_ID}&access_token=" . $_SESSION['accessToken'], true, 303);
 	}else{
 		header("Refresh:0; url=$CANONICAL_URL", true, 303);
 	}
--- pages/logout.php
+++ pages/logout.php
@@ -1,4 +1,5 @@
 <?php
 require "../config.php";
 session_start();
-header("Refresh:0; url={$OAUTH_CONCAT_BASE_URL}/oauth/logout?next={urlencode($CANONICAL_URL}&client_id={$OAUTH_CLIENT_ID}&access_token=" . $_SESSION['accessToken'], true, 303);
+$encoded_url = urlencode($CANONICAL_URL);
+header("Refresh:0; url={$OAUTH_CONCAT_BASE_URL}/oauth/logout?next=$encoded_url&client_id={$OAUTH_CLIENT_ID}&access_token=" . $_SESSION['accessToken'], true, 303);
