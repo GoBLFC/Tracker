349a694a67e5f642b574054e4b803e7a88cc32a4
Glitch <glitchthefurry@gmail.com>
2023-03-11 19:01:06 -0600

    Send header before content

--- logout.php
+++ logout.php
@@ -2,18 +2,15 @@
 
 require "main.php";
 
-logoutSession($session);
-
 setcookie("session", '', 0, "/");
 setcookie("badge", '', 0, "/");
 //header('Location: /');
 
 if (isset($_SESSION['accessToken'])) {
-    if ($devMode) { ?>
+    if ($devMode) {
+        header("Refresh:0; url=$CANONICAL_URL", true, 303); ?>
         <script>alert("Notice: Dev Mode is enabled. Signing out of Tracker will not sign you out of ConCat in this mode.");</script>
-        <?php
-        header("Refresh:0; url=$CANONICAL_URL", true, 303);
-    } else {
+    <?php } else {
         $encoded_url = urlencode($CANONICAL_URL);
         header("Refresh:0; url={$OAUTH_CONCAT_BASE_URL}/oauth/logout?next=$encoded_url&client_id={$OAUTH_CLIENT_ID}&access_token=" . $_SESSION['accessToken'], true, 303);
     }
@@ -25,4 +22,3 @@ session_unset();
 session_regenerate_id();
 
 ?>
-
