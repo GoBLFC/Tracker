6f977bdc63725875fb51cd9fbc11c2ed6ae5fed7
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-07-26 13:46:10 -0400

    Fix in-memory setting retrieval

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -28,7 +28,7 @@ class Setting extends Model {
 	 */
 	public static function getSetting(string $id, ?callable $transformer = null): mixed {
 		// Return the setting value from the in-memory cache if it exists there
-		if (isset(static::$settingsCache[$id])) return static::$settingsCache[$id]->value;
+		if (isset(static::$settingsCache[$id])) return static::$settingsCache[$id];
 
 		// Retrieve the setting value from the cache provider if it exists there, otherwise obtain it from the DB and cache it
 		$value = Cache::remember("setting:{$id}", 60 * 5, function () use ($id, $transformer) {
