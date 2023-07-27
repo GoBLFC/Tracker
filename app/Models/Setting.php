17818c6b32e1a292af65c85a9b0349530e26b363
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-07-27 00:18:10 -0400

    Require using setting-specific accessors

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -21,12 +21,26 @@ class Setting extends Model {
 	private static array $settingsCache = [];
 
 	/**
+	 * Retrieves and caches whether the app should be in dev mode
+	 */
+	public static function isDevMode(): bool {
+		return (bool) static::getAndCacheValue('dev-mode');
+	}
+
+	/**
+	 * Retrieves and caches the active event
+	 */
+	public static function activeEvent(): ?Event {
+		return static::getAndCacheValue('active-event', fn ($val) => Event::find($val));
+	}
+
+	/**
 	 * Retrieve the value of a setting from the in-memory cache, cache provider, or database, and cache it appropriately
 	 *
 	 * @param string $id
 	 * @param ?callable $transformer Function to mutate the setting value
 	 */
-	public static function getSetting(string $id, ?callable $transformer = null): mixed {
+	private static function getAndCacheValue(string $id, ?callable $transformer = null): mixed {
 		// Return the setting value from the in-memory cache if it exists there
 		if (isset(static::$settingsCache[$id])) return static::$settingsCache[$id];
 
@@ -40,19 +54,5 @@ class Setting extends Model {
 		// Store the setting value in the in-memory cache
 		static::$settingsCache[$id] = $value;
 		return $value;
-	}
-
-	/**
-	 * Retrieves and caches whether the app should be in dev mode
-	 */
-	public static function isDevMode(): bool {
-		return (bool) static::getSetting('dev-mode');
-	}
-
-	/**
-	 * Retrieves and caches the active event
-	 */
-	public static function activeEvent(): ?Event {
-		return static::getSetting('active-event', fn ($val) => Event::find($val));
 	}
 }
