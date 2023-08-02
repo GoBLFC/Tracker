1474a85ccd5c96f72cd87f39d7dbecbd8deb9a16
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-02 16:22:38 -0400

    Correct Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
 	plugins: [
 		laravel({
-			input: ['resources/css/app.css', 'resources/js/**/*', 'resources/img/**/*'],
+			input: [
+				'resources/css/app.css',
+				'resources/js/app.js',
+				'resources/js/login.js',
+				'resources/js/tracker.js',
+			],
 			refresh: true,
 		}),
 	],
