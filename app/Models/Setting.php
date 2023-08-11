204b7aa5d74c97a130f4824f52ac7c3488fb12b0
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-11 15:52:43 -0400

    Add Setting::set and make getters throw exceptions on failure

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -21,6 +21,24 @@ class Setting extends Model {
 	private static array $settingsCache = [];
 
 	/**
+	 * Stores a setting value in the database and clears any cached value for it
+	 * @return int Number of affected rows (should always be 1 on success)
+	 */
+	public static function set(string $id, Model|array|bool|int|float|string|null $value): void {
+		// Wipe the cache
+		unset(static::$settingsCache[$id]);
+		Cache::delete("setting:{$id}");
+
+		// Update the value
+		$count = static::whereId($id)->update([
+			'value' => json_encode($value instanceof Model ? $value->getKey() : $value)
+		]);
+
+		// Make sure the update went through - if it didn't, it's for an unknown setting
+		if ($count !== 1) throw new \ValueError("Unknown setting ID: {$id}");
+	}
+
+	/**
 	 * Retrieves and caches whether the app should be in dev mode
 	 */
 	public static function isDevMode(): bool {
@@ -46,7 +64,7 @@ class Setting extends Model {
 
 		// Retrieve the setting value from the cache provider if it exists there, otherwise obtain it from the DB and cache it
 		$value = Cache::remember("setting:{$id}", 60 * 5, function () use ($id, $transformer) {
-			$setting = static::find($id);
+			$setting = static::findOrFail($id);
 			if (!$setting || !$transformer) return $setting?->value;
 			return $transformer($setting->value);
 		});
