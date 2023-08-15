37301dd1adbf1335b87dbee21df036b019939e4e
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-15 05:35:33 -0400

    Add role middleware

---
+++ app/Http/Middleware/EnsureUserHasRole.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserHasRole {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+	 */
+	public function handle(Request $request, Closure $next, string $role): Response {
+		if ($request->user()->role->value < Role::fromName($role)->value) abort(403);
+		return $next($request);
+	}
+}
