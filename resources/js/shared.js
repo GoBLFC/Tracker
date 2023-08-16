b2e77208bf9837c54c5e0c8bffc2668a7b8635a8
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-16 03:20:14 -0400

    Reduce default toast duration to 4sec

--- resources/js/shared.js
+++ resources/js/shared.js
@@ -7,7 +7,7 @@ export const Toast = Swal.mixin({
     toast: true,
     position: "top-end",
     showConfirmButton: false,
-    timer: 5000,
+    timer: 4000,
     timerProgressBar: true,
     showClass: {
         popup: "animate__animated animate__slideInDown animate__faster"
