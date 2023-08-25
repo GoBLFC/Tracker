772fb75df096b0d8ab5c712e2413d48c5406be3e
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-25 06:17:45 -0400

    Enable query explaining on debugbar

--- config/debugbar.php
+++ config/debugbar.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Debugbar Settings
      |--------------------------------------------------------------------------
@@ -14,13 +14,13 @@ return [
      |
      */
 
-    'enabled' => env('DEBUGBAR_ENABLED', null),
-    'except' => [
-        'telescope*',
-        'horizon*',
-    ],
+	'enabled' => env('DEBUGBAR_ENABLED', null),
+	'except' => [
+		'telescope*',
+		'horizon*',
+	],
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Storage settings
      |--------------------------------------------------------------------------
@@ -32,17 +32,17 @@ return [
      | can also be used. For PDO, run the package migrations first.
      |
      */
-    'storage' => [
-        'enabled'    => true,
-        'driver'     => 'file', // redis, file, pdo, socket, custom
-        'path'       => storage_path('debugbar'), // For file driver
-        'connection' => null,   // Leave null for default connection (Redis/PDO)
-        'provider'   => '', // Instance of StorageInterface for custom driver
-        'hostname'   => '127.0.0.1', // Hostname to use with the "socket" driver
-        'port'       => 2304, // Port to use with the "socket" driver
-    ],
+	'storage' => [
+		'enabled'    => true,
+		'driver'     => 'file', // redis, file, pdo, socket, custom
+		'path'       => storage_path('debugbar'), // For file driver
+		'connection' => null,   // Leave null for default connection (Redis/PDO)
+		'provider'   => '', // Instance of StorageInterface for custom driver
+		'hostname'   => '127.0.0.1', // Hostname to use with the "socket" driver
+		'port'       => 2304, // Port to use with the "socket" driver
+	],
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Editor
     |--------------------------------------------------------------------------
@@ -56,9 +56,9 @@ return [
     |
     */
 
-    'editor' => env('DEBUGBAR_EDITOR', 'vscode'),
+	'editor' => env('DEBUGBAR_EDITOR', 'vscode'),
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Remote Path Mapping
     |--------------------------------------------------------------------------
@@ -81,10 +81,10 @@ return [
     |
     */
 
-    'remote_sites_path' => env('DEBUGBAR_REMOTE_SITES_PATH', ''),
-    'local_sites_path' => env('DEBUGBAR_LOCAL_SITES_PATH', ''),
+	'remote_sites_path' => env('DEBUGBAR_REMOTE_SITES_PATH', ''),
+	'local_sites_path' => env('DEBUGBAR_LOCAL_SITES_PATH', ''),
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Vendors
      |--------------------------------------------------------------------------
@@ -98,9 +98,9 @@ return [
      |
      */
 
-    'include_vendors' => true,
+	'include_vendors' => true,
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Capture Ajax Requests
      |--------------------------------------------------------------------------
@@ -114,10 +114,10 @@ return [
      | X-Requested-With with the value XMLHttpRequest (most JS libraries send this), or have application/json as a Accept header.
      */
 
-    'capture_ajax' => true,
-    'add_ajax_timing' => false,
+	'capture_ajax' => true,
+	'add_ajax_timing' => false,
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Custom Error Handler for Deprecated warnings
      |--------------------------------------------------------------------------
@@ -126,9 +126,9 @@ return [
      | in the Messages tab.
      |
      */
-    'error_handler' => false,
+	'error_handler' => false,
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Clockwork integration
      |--------------------------------------------------------------------------
@@ -137,9 +137,9 @@ return [
      | Extension, without the server-side code. It uses Debugbar collectors instead.
      |
      */
-    'clockwork' => false,
+	'clockwork' => false,
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | DataCollectors
      |--------------------------------------------------------------------------
@@ -148,33 +148,33 @@ return [
      |
      */
 
-    'collectors' => [
-        'phpinfo'         => true,  // Php version
-        'messages'        => true,  // Messages
-        'time'            => true,  // Time Datalogger
-        'memory'          => true,  // Memory usage
-        'exceptions'      => true,  // Exception displayer
-        'log'             => true,  // Logs from Monolog (merged in messages if enabled)
-        'db'              => true,  // Show database (PDO) queries and bindings
-        'views'           => true,  // Views with their data
-        'route'           => true,  // Current route information
-        'auth'            => true,  // Display Laravel authentication status
-        'gate'            => true,  // Display Laravel Gate checks
-        'session'         => true,  // Display session data
-        'symfony_request' => true,  // Only one can be enabled..
-        'mail'            => true,  // Catch mail messages
-        'laravel'         => true,  // Laravel version and environment
-        'events'          => true,  // All events fired
-        'default_request' => false, // Regular or special Symfony request logger
-        'logs'            => true,  // Add the latest log messages
-        'files'           => false, // Show the included files
-        'config'          => true,  // Display config settings
-        'cache'           => true,  // Display cache events
-        'models'          => true,  // Display models
-        'livewire'        => true,  // Display Livewire (when available)
-    ],
+	'collectors' => [
+		'phpinfo'         => true,  // Php version
+		'messages'        => true,  // Messages
+		'time'            => true,  // Time Datalogger
+		'memory'          => true,  // Memory usage
+		'exceptions'      => true,  // Exception displayer
+		'log'             => true,  // Logs from Monolog (merged in messages if enabled)
+		'db'              => true,  // Show database (PDO) queries and bindings
+		'views'           => true,  // Views with their data
+		'route'           => true,  // Current route information
+		'auth'            => true,  // Display Laravel authentication status
+		'gate'            => true,  // Display Laravel Gate checks
+		'session'         => true,  // Display session data
+		'symfony_request' => true,  // Only one can be enabled..
+		'mail'            => true,  // Catch mail messages
+		'laravel'         => true,  // Laravel version and environment
+		'events'          => true,  // All events fired
+		'default_request' => false, // Regular or special Symfony request logger
+		'logs'            => true,  // Add the latest log messages
+		'files'           => false, // Show the included files
+		'config'          => true,  // Display config settings
+		'cache'           => true,  // Display cache events
+		'models'          => true,  // Display models
+		'livewire'        => true,  // Display Livewire (when available)
+	],
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Extra options
      |--------------------------------------------------------------------------
@@ -183,44 +183,44 @@ return [
      |
      */
 
-    'options' => [
-        'auth' => [
-            'show_name' => true,   // Also show the users name/email in the debugbar
-        ],
-        'db' => [
-            'with_params'       => true,   // Render SQL with the parameters substituted
-            'backtrace'         => true,   // Use a backtrace to find the origin of the query in your files.
-            'backtrace_exclude_paths' => [],   // Paths to exclude from backtrace. (in addition to defaults)
-            'timeline'          => false,  // Add the queries to the timeline
-            'duration_background'  => true,   // Show shaded background on each query relative to how long it took to execute.
-            'explain' => [                 // Show EXPLAIN output on queries
-                'enabled' => false,
-                'types' => ['SELECT'],     // Deprecated setting, is always only SELECT
-            ],
-            'hints'             => false,    // Show hints for common mistakes
-            'show_copy'         => false,    // Show copy button next to the query,
-            'slow_threshold'    => false,   // Only track queries that last longer than this time in ms
-        ],
-        'mail' => [
-            'full_log' => false,
-        ],
-        'views' => [
-            'timeline' => true,  // Add the views to the timeline (Experimental)
-            'data' => true,    //Note: Can slow down the application, because the data can be quite large..
-            'exclude_paths' => [], // Add the paths which you don't want to appear in the views
-        ],
-        'route' => [
-            'label' => true,  // show complete route on bar
-        ],
-        'logs' => [
-            'file' => null,
-        ],
-        'cache' => [
-            'values' => true, // collect cache values
-        ],
-    ],
+	'options' => [
+		'auth' => [
+			'show_name' => true,   // Also show the users name/email in the debugbar
+		],
+		'db' => [
+			'with_params'       => true,   // Render SQL with the parameters substituted
+			'backtrace'         => true,   // Use a backtrace to find the origin of the query in your files.
+			'backtrace_exclude_paths' => [],   // Paths to exclude from backtrace. (in addition to defaults)
+			'timeline'          => false,  // Add the queries to the timeline
+			'duration_background'  => true,   // Show shaded background on each query relative to how long it took to execute.
+			'explain' => [                 // Show EXPLAIN output on queries
+				'enabled' => true,
+				'types' => ['SELECT'],     // Deprecated setting, is always only SELECT
+			],
+			'hints'             => false,    // Show hints for common mistakes
+			'show_copy'         => false,    // Show copy button next to the query,
+			'slow_threshold'    => false,   // Only track queries that last longer than this time in ms
+		],
+		'mail' => [
+			'full_log' => false,
+		],
+		'views' => [
+			'timeline' => true,  // Add the views to the timeline (Experimental)
+			'data' => true,    //Note: Can slow down the application, because the data can be quite large..
+			'exclude_paths' => [], // Add the paths which you don't want to appear in the views
+		],
+		'route' => [
+			'label' => true,  // show complete route on bar
+		],
+		'logs' => [
+			'file' => null,
+		],
+		'cache' => [
+			'values' => true, // collect cache values
+		],
+	],
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Inject Debugbar in Response
      |--------------------------------------------------------------------------
@@ -231,9 +231,9 @@ return [
      |
      */
 
-    'inject' => true,
+	'inject' => true,
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | DebugBar route prefix
      |--------------------------------------------------------------------------
@@ -243,9 +243,9 @@ return [
      | from trying to overcome bugs like this: http://trac.nginx.org/nginx/ticket/97
      |
      */
-    'route_prefix' => '_debugbar',
+	'route_prefix' => '_debugbar',
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | DebugBar route domain
      |--------------------------------------------------------------------------
@@ -253,9 +253,9 @@ return [
      | By default DebugBar route served from the same domain that request served.
      | To override default domain, specify it as a non-empty value.
      */
-    'route_domain' => null,
+	'route_domain' => null,
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | DebugBar theme
      |--------------------------------------------------------------------------
@@ -263,9 +263,9 @@ return [
      | Switches between light and dark theme. If set to auto it will respect system preferences
      | Possible values: auto, light, dark
      */
-    'theme' => env('DEBUGBAR_THEME', 'auto'),
+	'theme' => env('DEBUGBAR_THEME', 'auto'),
 
-    /*
+	/*
      |--------------------------------------------------------------------------
      | Backtrace stack limit
      |--------------------------------------------------------------------------
@@ -273,5 +273,5 @@ return [
      | By default, the DebugBar limits the number of frames returned by the 'debug_backtrace()' function.
      | If you need larger stacktraces, you can increase this number. Setting it to 0 will result in no limit.
      */
-    'debug_backtrace_limit' => 50,
+	'debug_backtrace_limit' => 50,
 ];
