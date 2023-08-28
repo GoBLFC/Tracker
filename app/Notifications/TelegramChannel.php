4b716c6ede6d606a4651b6bf107e265480209bdd
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-28 09:17:44 -0400

    Fix Telegram notifications throwing exceptions for users without Telegram linked

--- app/Notifications/TelegramChannel.php
+++ app/Notifications/TelegramChannel.php
@@ -7,6 +7,7 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramChannel {
 	public function send(object $notifiable, Notification $notification): void {
+		if (!$notifiable->tg_chat_id) return;
 		$message = $notification->toTelegram($notifiable);
 		Telegram::sendMessage(array_merge(['chat_id' => $notifiable->tg_chat_id], $message));
 	}
