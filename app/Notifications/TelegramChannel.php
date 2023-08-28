a99f69c64067e21870f691f3de4cf88ff0623cde
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-28 19:00:43 -0400

    Add notification type to the Telegram notification sent/skipped log messages

--- app/Notifications/TelegramChannel.php
+++ app/Notifications/TelegramChannel.php
@@ -8,15 +8,16 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramChannel {
 	public function send(object $notifiable, Notification $notification): void {
-		$type = get_class($notifiable);
+		$notifiableType = get_class($notifiable);
+		$notificationType = get_class($notification);
 
 		// Skip the notification if the user doesn't have a linked Telegram chat
 		if (!$notifiable->tg_chat_id) {
-			Log::info("Skipping Telegram notification {$notification->id} for {$type} {$notifiable->id}");
+			Log::info("Skipping Telegram notification {$notificationType} {$notification->id} for {$notifiableType} {$notifiable->id}");
 			return;
 		}
 
-		Log::info("Sending Telegram notification {$notification->id} for {$type} {$notifiable->id}");
+		Log::info("Sending Telegram notification {$notificationType} {$notification->id} for {$notifiableType} {$notifiable->id}");
 
 		$message = $notification->toTelegram($notifiable);
 		Telegram::sendMessage(array_merge(['chat_id' => $notifiable->tg_chat_id], $message));
