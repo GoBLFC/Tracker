eba0366dce27d20bd4acf4383432af2401e5e672
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-28 09:25:12 -0400

    Add logging for sent and skipped Telegram notifications

--- app/Notifications/TelegramChannel.php
+++ app/Notifications/TelegramChannel.php
@@ -2,12 +2,22 @@
 
 namespace App\Notifications;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Notifications\Notification;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramChannel {
 	public function send(object $notifiable, Notification $notification): void {
-		if (!$notifiable->tg_chat_id) return;
+		$type = get_class($notifiable);
+
+		// Skip the notification if the user doesn't have a linked Telegram chat
+		if (!$notifiable->tg_chat_id) {
+			Log::debug("Skipping Telegram notification {$notification->id} for {$type} {$notifiable->id}");
+			return;
+		}
+
+		Log::debug("Sending Telegram notification {$notification->id} for {$type} {$notifiable->id}");
+
 		$message = $notification->toTelegram($notifiable);
 		Telegram::sendMessage(array_merge(['chat_id' => $notifiable->tg_chat_id], $message));
 	}
