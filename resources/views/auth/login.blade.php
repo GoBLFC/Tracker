b15bafbcc6bf8a4833093a8c8bd0ce31e53c3570
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-29 03:30:09 -0400

    Improve login layout responsiveness

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="row justify-content-md-center mt-5">
-		<div class="col-md-4">
+	<div class="row justify-content-center mt-4 mt-md-5">
+		<div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
 			<div class="card mb-3">
 				<div class="card-body">
-					<h2 class="text-center">BLFC Volunteer Check-In</h2>
+					<h2 class="text-center mb-3">BLFC Volunteer Check-In</h2>
 					<div class="alert alert-light text-center" role="alert">Welcome! Click below to sign in.</div>
 					<a class="btn btn-success d-block mb-3" href="{!! route('auth.redirect') !!}" role="button">Sign In</a>
 
 					<div class="card mt-5">
-						<h5 class="card-header text-center">
+						<h3 class="h5 card-header text-center">
 							Quick Sign-in Code <span data-bs-toggle="tooltip" data-bs-title="Link your Telegram account after you sign in above to get a quick code anytime!"><i class="fa-regular fa-circle-question"></i></span>
-						</h5>
+						</h3>
 						<div class="card-body">
 							<form id="form" autocomplete="off">
 								<div class="input-group input-group-lg mb-3">
@@ -31,8 +31,8 @@
 	</div>
 @endsection
 
-@section('footer-class', 'row justify-content-md-center')
-@section('footer-nav-class', 'col-md-4')
+@section('footer-class', 'row justify-content-sm-center')
+@section('footer-nav-class', 'col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4')
 
 @push('modules')
 	@vite('resources/js/login.js')
