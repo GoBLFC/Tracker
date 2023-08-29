3e7a7983b54987806f71eafadc56d7170d0c7241
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-29 01:10:36 -0400

    Add kiosk description to lead and manager pages

--- resources/views/management/manage.blade.php
+++ resources/views/management/manage.blade.php
@@ -285,8 +285,22 @@
 
 			<div class="card mb-4">
 				<div class="card-header">Kiosk Settings</div>
-				<div class="card-body text-center">
-					@include('partials.toggle-kiosk-button')
+				<div class="card-body">
+					<dl class="mb-0">
+						{{-- Kiosk toggle --}}
+						<div class="row">
+							<dt class="col-xl-4 col-md-4 col-sm-12 mb-2 mb-md-0 align-self-center">
+								@include('partials.toggle-kiosk-button', ['kioskToggleClasses' => 'btn float-md-end'])
+							</dt>
+							<dd class="col-xl-6 col-md-8 col-sm-12 mb-0">
+								<p class="mb-0">
+									Authorizing this device as a kiosk will allow volunteers to check in or out on this device.
+									This is required when setting up dedicated devices pre-con for checking in or out.
+									Kiosks remain authorized for {!! Carbon\CarbonInterval::minutes(config('tracker.kiosk_lifetime'))->cascade()->forHumans() !!}.
+								</p>
+							</dd>
+						</div>
+					</dl>
 				</div>
 			</div>
 
