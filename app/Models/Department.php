60757d6b9afde3caa969dc273b9e37e036857457
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-30 03:05:15 -0400

    Remove redundant Department ID declaration in PHPdoc

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
- * @property string $id
  * @property string $name
  * @property bool $hidden
  * @property \Illuminate\Support\Carbon|null $created_at
