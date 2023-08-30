54519e1a663323abac19b2686fa3229378ac5352
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-30 17:46:43 -0400

    Catch errors when notifying old linked Telegram chat of link change

--- app/Telegram/Commands/LinkCommand.php
+++ app/Telegram/Commands/LinkCommand.php
@@ -3,6 +3,8 @@
 namespace App\Telegram\Commands;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Exceptions\TelegramSDKException;
 use Telegram\Bot\Keyboard\Keyboard;
 
 class LinkCommand extends Command {
@@ -47,13 +49,23 @@ class LinkCommand extends Command {
 		}
 
 		// If the user already has an associated chat, inform them it's being moved
+		// We catch and log any errors here so that if the old chat no longer exists, the new link still goes through
 		if ($user->tg_chat_id) {
-			$this->telegram->sendMessage([
-				'chat_id' => $user->tg_chat_id,
-				'text' => "I have been changed to report to another user.\nYou'll need to scan a new QR code (at the volunteer desk or the {$trackerLink}) to continue to get your volunteer time info from me.",
-				'parse_mode' => 'HTML',
-				'reply_markup' => Keyboard::remove(),
-			]);
+			try {
+				$this->telegram->sendMessage([
+					'chat_id' => $user->tg_chat_id,
+					'text' => "I have been changed to report to another user.\nYou'll need to scan a new QR code (at the volunteer desk or the {$trackerLink}) to continue to get your volunteer time info from me.",
+					'parse_mode' => 'HTML',
+					'reply_markup' => Keyboard::remove(),
+				]);
+			} catch (TelegramSDKException $err) {
+				Log::warning('Error notifying old Telegram chat for linkage change', [
+					'oldChat' => $user->tg_chat_id,
+					'newChat' => $chatId,
+					'user' => $user->id,
+					'error' => $err,
+				]);
+			}
 		}
 
 		// Store the chat ID and regenerate the setup key (to prevent reuse)
