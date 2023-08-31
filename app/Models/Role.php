108e7359fe1b2101d0ab1f61914ce18eca8728b2
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-31 16:49:14 -0400

    Move Role string maps into static arrays

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,40 +12,58 @@ enum Role: int {
 	case Banned = -1;
 
 	/**
-	 * Gets the Bootstrap color class (like "danger" or "warning") for the case
+	 * Get the description for the case
+	 */
+	public function description(): string {
+		return static::descriptions[$this->value];
+	}
+
+	/**
+	 * Get the Bootstrap color class (like "danger" or "warning") for the case
 	 */
 	public function colorClass(): string {
-		return [
-			3 => 'danger',
-			2 => 'warning',
-			1 => 'success',
-			0 => 'info',
-			-1 => 'danger',
-		][$this->value];
+		return static::colorClasses[$this->value];
 	}
 
 	/**
 	 * Get the action label (like "Make Admin" or "BAN") for the case
 	 */
 	public function actionLabel(): string {
-		return [
-			3 => 'Make Admin',
-			2 => 'Make Manager',
-			1 => 'Make Lead',
-			0 => 'Make Volunteer',
-			-1 => 'BAN',
-		][$this->value];
+		return static::actionLabels[$this->value];
 	}
 
-	public function description(): string {
-		return [
-			3 => 'Administrators can do anything.',
-			2 => 'Managers can view, edit, and create time entries on behalf of other users, as well as create new users with a badge ID, authorize kiosks, and bypass the lockdown.',
-			1 => 'Leads can authorize kiosks.',
-			0 => 'Volunteers can only check in and out for shifts.',
-			-1 => 'Banned users can\'t do anything.',
-		][$this->value];
-	}
+	/**
+	 * Array of case values to descriptions for the corresponding role
+	 */
+	public const descriptions = [
+		3 => 'Administrators can do anything.',
+		2 => 'Managers can view, edit, and create time entries on behalf of other users, as well as create new users with a badge ID, authorize kiosks, and bypass the lockdown.',
+		1 => 'Leads can authorize kiosks.',
+		0 => 'Volunteers can only check in and out for shifts.',
+		-1 => 'Banned users can\'t do anything.',
+	];
+
+	/**
+	 * Array of case values to action labels for the corresponding role
+	 */
+	public const actionLabels = [
+		3 => 'Make Admin',
+		2 => 'Make Manager',
+		1 => 'Make Lead',
+		0 => 'Make Volunteer',
+		-1 => 'BAN',
+	];
+
+	/**
+	 * Array of case values to color classes for the corresponding role
+	 */
+	public const colorClasses = [
+		3 => 'danger',
+		2 => 'warning',
+		1 => 'success',
+		0 => 'info',
+		-1 => 'danger',
+	];
 
 	/**
 	 * Get the enumeration case with the specified name
