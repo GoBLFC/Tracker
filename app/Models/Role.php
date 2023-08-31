0d2313085fde44b731e3025dda0140b24196ae5d
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-08-31 05:04:35 -0400

    Add tidbit about lockdown bypassing to manager description

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -40,7 +40,7 @@ enum Role: int {
 	public function description(): string {
 		return [
 			3 => 'Administrators can do anything.',
-			2 => 'Managers can view, edit, and create time entries on behalf of other users, as well as create new users with a badge ID and authorize kiosks.',
+			2 => 'Managers can view, edit, and create time entries on behalf of other users, as well as create new users with a badge ID, authorize kiosks, and bypass the lockdown.',
 			1 => 'Leads can authorize kiosks.',
 			0 => 'Volunteers can only check in and out for shifts.',
 			-1 => 'Banned users can\'t do anything.',
