834cfb50c3be8bac2daf59dd5de1bf7d9c6f65cb
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-09-02 04:34:27 -0400

    Add placeholder for admin reports page

---
+++ resources/views/admin/reports.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('admin-content')
+	<div class="card mb-3">
+		<h4 class="card-header">Reports</h4>
+		<div class="card-body">
+			<p class="mb-0">Glitch is cute. 🥖</p>
+		</div>
+	</div>
+@endsection
