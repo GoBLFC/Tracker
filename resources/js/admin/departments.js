5bb46a8f9f6e608ab5a59538e0f1ccbe2d7065f6
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-09-02 02:29:32 -0400

    Teensy JS optimization

--- resources/js/admin/departments.js
+++ resources/js/admin/departments.js
@@ -7,8 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	const updateForms = document.querySelectorAll('form.update');
 	for(const form of updateForms) {
 		const updateBtn = form.querySelector('button[type="submit"]');
-		const nameIpt = form.closest('tr').querySelector('input[name="name"]');
-		const hideChk = form.closest('tr').querySelector('input[type="checkbox"]');
+		const row = form.closest('tr');
+		const nameIpt = row.querySelector('input[name="name"]');
+		const hideChk = row.querySelector('input[type="checkbox"]');
 
 		updateBtn.disabled = true;
 		nameIpt.addEventListener('input', () => { updateBtn.disabled = false; });
