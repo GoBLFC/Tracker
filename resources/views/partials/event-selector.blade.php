30e8c64191170a0eeeb008917b9ff3678016bc93
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-09-02 01:17:08 -0400

    Add text to event selector when no events exist

--- resources/views/partials/event-selector.blade.php
+++ resources/views/partials/event-selector.blade.php
@@ -1,17 +1,23 @@
 <div class="card mb-4">
 	<div class="card-header">Edit for Event</div>
 	<div class="card-body">
-		<div class="input-group">
-			<label for="eventSelector" class="input-group-text">Event</label>
-			<select id="eventSelector" class="form-control" data-route="{!! $route !!}">
-				@if(!$event) <option value="" selected disabled hidden>Select an Event</option> @endif
-				@foreach($events as $otherEvent)
-					<option value="{!! $otherEvent->id !!}" {!! $otherEvent->id === $event?->id ? 'selected' : '' !!}>
-						{{ $otherEvent->name }}
-					</option>
-				@endforeach
-			</select>
-		</div>
+		@if($events)
+			<div class="input-group">
+				<label for="eventSelector" class="input-group-text">Event</label>
+				<select id="eventSelector" class="form-control" data-route="{!! $route !!}">
+					@if(!$event) <option value="" selected disabled hidden>Select an Event</option> @endif
+					@foreach($events as $otherEvent)
+						<option value="{!! $otherEvent->id !!}" {!! $otherEvent->id === $event?->id ? 'selected' : '' !!}>
+							{{ $otherEvent->name }}
+						</option>
+					@endforeach
+				</select>
+			</div>
+		@else
+			<p class="mb-0">
+				There aren't any events yet - you'll need to <a href="{!! route('admin.events') !!}">create one</a> to manage.
+			</p>
+		@endif
 	</div>
 </div>
 
