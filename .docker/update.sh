869711895b22aaa5f50f5c093c6d842375bda13c
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-09-02 01:44:54 -0400

    Add permission fixing to update script

--- .docker/update.sh
+++ .docker/update.sh
@@ -5,6 +5,7 @@ git pull
 docker compose -f docker-compose.prod.yml build
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml exec app chown -R www-data:www-data /var/www/html/storage
 docker compose -f docker-compose.prod.yml exec app php artisan down
 docker compose -f docker-compose.prod.yml exec app php artisan migrate
 docker compose -f docker-compose.prod.yml exec app php artisan up
