b100c7c41d075f99d0f5e50da8099e5f4168aaf1
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-10-29 17:42:07 -0400

    Move user entry cards above attendee log list

--- resources/views/attendee-logs/view.blade.php
+++ resources/views/attendee-logs/view.blade.php
@@ -18,7 +18,47 @@
 
 			@php($readOnly = !$attendeeLog->isForActiveEvent() && !Auth::user()->isAdmin())
 
-			<div class="card mb-3">
+			@if(!$readOnly)
+				<div class="card mb-3">
+					<h5 class="card-header">Log Attendee</h5>
+					<div class="card-body">
+						<form action="{!! route('attendee-logs.users.store', $attendeeLog->id) !!}" method="POST" id="userLog" class="seamless">
+							@method('PUT')
+							@csrf
+							<div class="input-group">
+								<label for="badgeId" class="input-group-text">Badge ID</label>
+								<input type="text" inputmode="numeric" pattern="[0-9]+" name="badge_id" id="logBadgeId" class="form-control" required autofocus />
+								<button type="submit" class="btn btn-success" data-success="Logged attendee.">Log</button>
+							</div>
+						</form>
+					</div>
+				</div>
+
+				@manager
+					<div class="card mb-3">
+						<h5 class="card-header">Add Gatekeeper</h5>
+						<div class="card-body">
+							<p>
+								Gatekeepers can view, add, and delete attendees in the log, but cannot manage gatekeepers themselves.
+								Any volunteer, not just staff, can be added as a gatekeeper.
+							</p>
+
+							<form action="{!! route('attendee-logs.users.store', $attendeeLog->id) !!}" method="POST" id="gatekeeperAdd" class="seamless">
+								@method('PUT')
+								@csrf
+								<input type="hidden" name="type" value="gatekeeper" />
+								<div class="input-group">
+									<label for="badgeId" class="input-group-text">Badge ID</label>
+									<input type="text" inputmode="numeric" pattern="[0-9]+" name="badge_id" id="gatekeeperBadgeId" class="form-control" required autofocus />
+									<button type="submit" class="btn btn-warning" data-success="Empowered gatekeeper.">Empower Gatekeeper</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				@endmanager
+			@endif
+
+			<div class="card">
 				<h5 class="card-header">{{ $attendeeLog->display_name }}</h5>
 				<div id="users-body" class="card-body p-0 {!! $attendeeLog->users->isEmpty() ? 'd-none' : '' !!}">
 					<div class="table-responsive">
@@ -70,46 +110,6 @@
 				</div>
 				<p id="users-body-empty" class="card-body mb-0 {!! $attendeeLog->users->isNotEmpty() ? 'd-none' : '' !!}">There are no attendees logged.</p>
 			</div>
-
-			@if(!$readOnly)
-				<div class="card">
-					<h5 class="card-header">Log Attendee</h5>
-					<div class="card-body">
-						<form action="{!! route('attendee-logs.users.store', $attendeeLog->id) !!}" method="POST" id="userLog" class="seamless">
-							@method('PUT')
-							@csrf
-							<div class="input-group">
-								<label for="badgeId" class="input-group-text">Badge ID</label>
-								<input type="text" inputmode="numeric" pattern="[0-9]+" name="badge_id" id="logBadgeId" class="form-control" required autofocus />
-								<button type="submit" class="btn btn-success" data-success="Logged attendee.">Log</button>
-							</div>
-						</form>
-					</div>
-				</div>
-
-				@manager
-					<div class="card mt-3">
-						<h5 class="card-header">Add Gatekeeper</h5>
-						<div class="card-body">
-							<p>
-								Gatekeepers can view, add, and delete attendees in the log, but cannot manage gatekeepers themselves.
-								Any volunteer, not just staff, can be added as a gatekeeper.
-							</p>
-
-							<form action="{!! route('attendee-logs.users.store', $attendeeLog->id) !!}" method="POST" id="gatekeeperAdd" class="seamless">
-								@method('PUT')
-								@csrf
-								<input type="hidden" name="type" value="gatekeeper" />
-								<div class="input-group">
-									<label for="badgeId" class="input-group-text">Badge ID</label>
-									<input type="text" inputmode="numeric" pattern="[0-9]+" name="badge_id" id="gatekeeperBadgeId" class="form-control" required autofocus />
-									<button type="submit" class="btn btn-warning" data-success="Empowered gatekeeper.">Empower Gatekeeper</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				@endmanager
-			@endif
 
 			<a class="btn btn-primary float-end mt-3" href="{!! route('events.attendee-logs.index', $attendeeLog->event->id) !!}" role="button">Back</a>
 		</div>
