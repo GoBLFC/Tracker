b9d1d9e3ef278adaf343f734ff407e33c11e2764
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-10-08 17:55:22 -0400

    Add Inertia-based error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
 	->withRouting(
@@ -34,5 +36,18 @@ return Application::configure(basePath: dirname(__DIR__))
 		$middleware->throttleApi();
 	})
 	->withExceptions(function (Exceptions $exceptions) {
-		//
+		$exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+			$isLocal = app()->environment(['local', 'testing']);
+			$isHandledStatus = in_array($response->getStatusCode(), [500, 503, 404, 403]);
+
+			if ($isLocal && $isHandledStatus) {
+				return Inertia::render('Error', ['status' => $response->getStatusCode()])
+					->toResponse($request)
+					->setStatusCode($response->getStatusCode());
+			} elseif ($response->getStatusCode() === 419) {
+				return back()->with(['error' => 'The page expired, please try again.']);
+			}
+
+			return $response;
+		});
 	})->create();
