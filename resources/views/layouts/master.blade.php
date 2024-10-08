db7a6abe14f231ae6f0e213785e0fc148569af49
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-10-08 14:01:29 -0400

    Update all frontend & backend dependencies

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
 	<link rel="icon" sizes="192x192" href="{!! Vite::asset('resources/img/blfc-chip-192.png') !!}" />
 
 	@prepend('styles')
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		@vite('resources/sass/app.scss')
 	@endprepend
 	@stack('styles')
