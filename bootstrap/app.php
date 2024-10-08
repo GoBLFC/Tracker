d84cc56c751216b703012ab67049e0a066f8430d
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-10-08 14:05:22 -0400

    Update Laravel skeleton version number

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-// Current Laravel skeleton version: v11.2.0
+// Current Laravel skeleton version: v11.2.1
 
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
