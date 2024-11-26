cebd6d6d9ce08b897eafbdd1a91927c8aab661d8
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-11-25 23:39:32 -0500

    Add correct hostname to temp cert

--- .docker/nginx/generate-temp-cert.sh
+++ .docker/nginx/generate-temp-cert.sh
@@ -7,5 +7,6 @@ if [ ! -d /etc/letsencrypt/live/${NGINX_HOST} ]; then
 	openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
 		-keyout "/etc/letsencrypt/live/${NGINX_HOST}/privkey.pem" \
 		-out "/etc/letsencrypt/live/${NGINX_HOST}/fullchain.pem" \
-		-subj '/CN=localhost'
+		-subj "/CN=${NGINX_HOST}" \
+		-addext "subjectAltName = DNS:localhost"
 fi
