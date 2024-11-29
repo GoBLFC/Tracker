9e66bb7de70d0adf192ad76afaf4b5ba3f3d8869
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-11-28 20:19:53 -0500

    Fix ResponsiveTag content not rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,5 @@ import TailwindPrimeUi from 'tailwindcss-primeui';
 export default {
 	content: ['./resources/js/**/*.vue', './resources/views/**/*.blade.php'],
 	plugins: [TailwindPrimeUi],
+	safelist: ['sm:not-sr-only', 'md:not-sr-only', 'lg:not-sr-only', 'xl:not-sr-only'],
 };
