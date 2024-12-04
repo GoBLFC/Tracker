11674ecc0daec5b5807178eb40ccb6015ca52546
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-12-04 02:24:30 -0500

    Update scripts for prebuilt images

--- .docker/scripts/update.sh
+++ .docker/scripts/update.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 cd "$(dirname "$0")/../.."
-git pull
-docker compose -f docker-compose.prod.yml build
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d
-docker compose -f docker-compose.prod.yml exec app chown -R www-data:www-data /var/www/html/storage
-docker compose -f docker-compose.prod.yml exec app php artisan down
-docker compose -f docker-compose.prod.yml exec app php artisan migrate
-docker compose -f docker-compose.prod.yml exec app php artisan up
+docker compose pull
+docker compose exec app php artisan down
+docker compose down
+docker compose up -d
+docker compose exec app chown -R www-data:www-data /var/www/html/storage
+docker compose exec app php artisan migrate
+docker compose exec app php artisan up
