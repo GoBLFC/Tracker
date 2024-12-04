b38fe347296cc2cf8099dca1e5fff1358b32c9af
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-12-04 02:24:30 -0500

    Update scripts for prebuilt images

--- .docker/scripts/postgres-dump.sh
+++ .docker/scripts/postgres-dump.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd "$(dirname "$0")/../.."
-source .docker/env/postgres.env
-docker compose -f docker-compose.prod.yml exec postgres pg_dumpall -U $POSTGRES_USER > pgdump.sql
+source .docker/postgres.env
+docker compose exec postgres pg_dumpall -U $POSTGRES_USER > pgdump.sql
