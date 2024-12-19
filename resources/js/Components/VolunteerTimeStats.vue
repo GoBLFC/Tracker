448e10723326c6c0e8afa53046900f258969bdba
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-12-19 18:45:13 -0500

    Add help icons to time stat labels

--- resources/js/Components/VolunteerTimeStats.vue
+++ resources/js/Components/VolunteerTimeStats.vue
@@ -2,17 +2,23 @@
 	<div
 		class="flex flex-wrap px-4 py-6 justify-around gap-8 lg:gap-16 text-center"
 	>
-		<div v-if="ongoing">
+		<div v-if="ongoing" class="flex flex-col gap-1">
 			<div
 				class="flex flex-col lg:flex-row items-center gap-3 text-2xl md:text-4xl"
 			>
 				<FontAwesomeIcon :icon="faClock" />
 				<Duration format="clock" :start="ongoing.start" :now />
 			</div>
-			<div class="uppercase text-muted-color">Shift Duration</div>
+			<div class="uppercase text-muted-color">
+				Shift Duration
+				<HelpIcon
+					text="Time clocked so far in the current shift. This doesn't include any bonuses that may apply."
+					class="ms-1"
+				/>
+			</div>
 		</div>
 
-		<div>
+		<div class="flex flex-col gap-1">
 			<div
 				class="flex flex-col lg:flex-row items-center gap-3 text-2xl md:text-4xl"
 			>
@@ -23,10 +29,16 @@
 					:now="ongoing ? now : undefined"
 				/>
 			</div>
-			<div class="uppercase text-muted-color">Time Today</div>
+			<div class="uppercase text-muted-color">
+				Time Today
+				<HelpIcon
+					text="Time clocked for the current day. This doesn't include any bonuses that may apply."
+					class="ms-1"
+				/>
+			</div>
 		</div>
 
-		<div>
+		<div class="flex flex-col gap-1">
 			<div
 				class="flex flex-col lg:flex-row items-center gap-3 text-2xl md:text-4xl"
 			>
@@ -37,7 +49,13 @@
 					:now="ongoing ? now : undefined"
 				/>
 			</div>
-			<div class="uppercase text-muted-color">Time Earned</div>
+			<div class="uppercase text-muted-color">
+				Time Earned
+				<HelpIcon
+					text="Total time earned during the entire event, including any bonuses that may apply"
+					class="ms-1"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
@@ -48,6 +66,7 @@ import type VolunteerTime from '../data/VolunteerTime';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
+import HelpIcon from './HelpIcon.vue';
 import Duration from './Duration.vue';
 
 const { time } = defineProps<{
