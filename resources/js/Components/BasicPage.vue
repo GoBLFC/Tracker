4110858bb483ab410ac4c4185db27736679d6c46
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-12-20 00:11:58 -0500

    Extract skeleton of banned page into a reusable component

---
+++ resources/js/Components/BasicPage.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="h-full flex flex-col justify-center items-center gap-8">
+		<Head :title />
+
+		<FontAwesomeIcon class="text-6xl" :icon />
+
+		<h1 class="text-3xl text-center font-light">{{ title }}</h1>
+
+		<Message :severity>
+			<slot />
+		</Message>
+	</div>
+</template>
+
+<script setup lang="ts">
+import type { MessageProps } from 'primevue/message';
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const { severity = 'secondary' } = defineProps<{
+	title: string;
+	icon: IconDefinition;
+	severity?: MessageProps['severity'];
+}>();
+</script>
--- resources/js/Pages/Suspended.vue
+++ resources/js/Pages/Suspended.vue
@@ -1,20 +1,15 @@
 <template>
-	<div class="h-full flex flex-col justify-center items-center gap-8">
-		<Head title="Not Permitted" />
-
-		<FontAwesomeIcon class="text-6xl" :icon="faTriangleExclamation" />
-
-		<h1 class="text-3xl text-center font-light">Not Permitted</h1>
-
-		<Message severity="warn">
-			There is a hold on your volunteer account. Please talk to a
-			Volunteer Manager at the volunteer desk.
-		</Message>
-	</div>
+	<BasicPage
+		title="Not Permitted"
+		severity="warn"
+		:icon="faTriangleExclamation"
+	>
+		There is a hold on your volunteer account. Please talk to a Volunteer
+		Manager at the volunteer desk.
+	</BasicPage>
 </template>
 
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import BasicPage from '../Components/BasicPage.vue';
 </script>
