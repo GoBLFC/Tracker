2e97481de5bc6a699dcf503b8336f44e82931c5d
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-01-03 12:22:08 -0500

    Fix maintenance typo

--- resources/js/Pages/Maintenance.vue
+++ resources/js/Pages/Maintenance.vue
@@ -1,10 +1,10 @@
 <template>
-	<BasicPage title="Maintenace" :icon="faHardHat">
+	<BasicPage title="Maintenance" :icon="faHardHat">
 		The site is disabled for maintenance. Check back again shortly.
 	</BasicPage>
 </template>
 
 <script setup lang="ts">
-import { faHardHat } from '@fortawesome/free-solid-svg-icons';
-import BasicPage from '@/Components/App/BasicPage.vue';
+import { faHardHat } from "@fortawesome/free-solid-svg-icons";
+import BasicPage from "@/Components/App/BasicPage.vue";
 </script>
