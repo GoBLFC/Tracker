57aaf4af0ce0ac8883852787b8322e67858257f6
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-01-06 11:35:04 -0500

    Gracefully handle missing auth property in useUser()

--- resources/js/lib/user.ts
+++ resources/js/lib/user.ts
@@ -9,25 +9,25 @@ export function useUser() {
 	const page = usePage<SharedProps>();
 
 	return {
-		id: toRef(() => page.props.auth.user?.id),
-		badgeId: toRef(() => page.props.auth.user?.badge_id),
-		username: toRef(() => page.props.auth.user?.username),
-		badgeName: toRef(() => page.props.auth.user?.badge_name),
-		displayName: toRef(() => page.props.auth.user?.badge_name ?? page.props.auth.user?.username),
-		role: toRef(() => page.props.auth.user?.role),
+		id: toRef(() => page.props.auth?.user?.id),
+		badgeId: toRef(() => page.props.auth?.user?.badge_id),
+		username: toRef(() => page.props.auth?.user?.username),
+		badgeName: toRef(() => page.props.auth?.user?.badge_name),
+		displayName: toRef(() => page.props.auth?.user?.badge_name ?? page.props.auth?.user?.username),
+		role: toRef(() => page.props.auth?.user?.role),
 		roleName: toRef(() =>
-			page.props.auth.user?.role || page.props.auth.user?.role === 0
-				? roleNames[page.props.auth.user.role]
+			page.props.auth?.user?.role || page.props.auth?.user?.role === 0
+				? roleNames[page.props.auth?.user.role]
 				: null,
 		),
 
-		isLoggedIn: toRef(() => Boolean(page.props.auth.user)),
+		isLoggedIn: toRef(() => Boolean(page.props.auth?.user)),
 		isGatekeeper: toRef(() => page.props.isGatekeeper),
-		isBanned: toRef(() => page.props.auth.user?.role === -2),
-		isStaff: toRef(() => (page.props.auth.user?.role ?? 0) >= 1),
-		isLead: toRef(() => (page.props.auth.user?.role ?? 0) >= 2),
-		isManager: toRef(() => (page.props.auth.user?.role ?? 0) >= 3),
-		isAdmin: toRef(() => page.props.auth.user?.role === 4),
+		isBanned: toRef(() => page.props.auth?.user?.role === -2),
+		isStaff: toRef(() => (page.props.auth?.user?.role ?? 0) >= 1),
+		isLead: toRef(() => (page.props.auth?.user?.role ?? 0) >= 2),
+		isManager: toRef(() => (page.props.auth?.user?.role ?? 0) >= 3),
+		isAdmin: toRef(() => page.props.auth?.user?.role === 4),
 	};
 }
 
