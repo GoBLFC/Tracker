aad4c855162a7e4c32e0589cf4d83100d2182754
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-04-16 01:13:34 -0400

    Remove unnecessary manual asset import

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,6 @@
 
 	@prepend('styles')
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-		@vite('resources/sass/app.scss')
 	@endprepend
 	@stack('styles')
 
