37e914e9417abed3b1b4ce2a81f6e8b52826307f
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-06-03 19:40:17 -0400

    Simplify certbot runner multi domain

--- .docker/certbot/certbot-runner.sh
+++ .docker/certbot/certbot-runner.sh
@@ -5,20 +5,13 @@ if [ ! -f /etc/letsencrypt/installed ]; then
 	echo "First run; running certbot certonly in 30 seconds"
 	sleep 30s
 
-	args=""
-	OIFS=$IFS; IFS=", "
-	for domain in $LETSENCRYPT_DOMAINS; do
-		args+=" -d $domain"
-	done
-	IFS=$OIFS
-
 	rm -rf /etc/letsencrypt/live
 	certbot certonly --non-interactive \
 		${LETSENCRYPT_DRY_RUN:+--dry-run} \
 		--agree-tos \
 		--email ${LETSENCRYPT_EMAIL} \
 		--webroot -w /var/www/certbot \
-		${args} \
+		--domains "$LETSENCRYPT_DOMAINS" \
 	&& touch /etc/letsencrypt/installed
 fi
 
