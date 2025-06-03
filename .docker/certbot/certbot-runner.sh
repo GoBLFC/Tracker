1f9907a7a5948e295825631d596ecc355c2daaf4
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-06-03 19:46:25 -0400

    Add compat for old env var to certbot runner

--- .docker/certbot/certbot-runner.sh
+++ .docker/certbot/certbot-runner.sh
@@ -5,13 +5,15 @@ if [ ! -f /etc/letsencrypt/installed ]; then
 	echo "First run; running certbot certonly in 30 seconds"
 	sleep 30s
 
+
+
 	rm -rf /etc/letsencrypt/live
 	certbot certonly --non-interactive \
 		${LETSENCRYPT_DRY_RUN:+--dry-run} \
 		--agree-tos \
 		--email ${LETSENCRYPT_EMAIL} \
 		--webroot -w /var/www/certbot \
-		--domains "$LETSENCRYPT_DOMAINS" \
+		--domains "${LETSENCRYPT_DOMAINS:-$LETSENCRYPT_DOMAIN}" \
 	&& touch /etc/letsencrypt/installed
 fi
 
