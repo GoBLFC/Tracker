bc4c29d147815359be4f620e06ef064edc9ecb56
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-06-24 20:36:14 +0000

    Disable debugbar mail collector

--- config/debugbar.php
+++ config/debugbar.php
@@ -175,7 +175,7 @@ return [
 		'gate' => env('DEBUGBAR_COLLECTORS_GATE', true),             // Display Laravel Gate checks
 		'session' => env('DEBUGBAR_COLLECTORS_SESSION', false),         // Display session data
 		'symfony_request' => env('DEBUGBAR_COLLECTORS_SYMFONY_REQUEST', true),  // Only one can be enabled..
-		'mail' => env('DEBUGBAR_COLLECTORS_MAIL', true),             // Catch mail messages
+		'mail' => env('DEBUGBAR_COLLECTORS_MAIL', false),             // Catch mail messages
 		'laravel' => env('DEBUGBAR_COLLECTORS_LARAVEL', true),          // Laravel version and environment
 		'events' => env('DEBUGBAR_COLLECTORS_EVENTS', true),          // All events fired
 		'default_request' => env('DEBUGBAR_COLLECTORS_DEFAULT_REQUEST', false), // Regular or special Symfony request logger
